1880d813a0e162054eca31230e8a06323c8abe6a
Álvaro Pereira <mutachuzkayros@gmail.com>
2021-08-26 12:18:30 -0700

    Initial commit

---
+++ AgendaRestApi/Controllers/PingController.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AgendaRestApi.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class PingController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            return Ok(new
+            {
+                pong = true
+            });
+        }
+    }
+}
